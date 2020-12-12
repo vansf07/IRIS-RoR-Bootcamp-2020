75920d383a20306f0b4186dc7413721c89f33a8a
Aritra Sinha <aritrasinha.191ec108@nitk.edu.in>
2020-12-12 15:50:44 +0530

    Solution for session_1

--- hello_world/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get '/', to: 'page#root'
-  get 'about_me', to: 'page#about_me'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
